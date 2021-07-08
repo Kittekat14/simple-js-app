3d51db814b0a07db8abf265d95781c6bb7b2d312
Kittekat14 <katihofstetter@gmail.com>
2021-07-08 12:52:42 +0200

    add objects of pokemons as an array

--- js/script.js
+++ js/script.js
@@ -1,9 +1,59 @@
-alert('Hello world');
+let pokemonList = [
+  {
+    name: 'Charmander', 
+    height: 0.6,
+    type: 'Fire',
+    eggGroup: ['Monster', 'Dragon'],
+    ability: ['Blaze','Solar-power']
+  },
+  {
+    name: 'Squirtle',
+    height: 0.5,
+    type: 'Water',
+    eggGroup: ['Monster', 'Water'],
+    ability: ['Rain-dish', 'Torrent']
+  },
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    type: ['Grass', 'Poison'],
+    eggGroup: ['Monster', 'Grass'],
+    ability: ['Chlorophyll', 'Overgrow']
+  },
+  {
+    name: 'Butterfree',
+    height: 1.1,
+    type: ['Bug', 'Flying'],
+    eggGroup: 'Bug',
+    ability: ['Compoundeyes', 'Tinted-lens']
+  },
+  {
+    name: 'Vulpix',
+    height: 0.6,
+    type: 'Fire',
+    eggGroup: 'Field',
+    ability:  ['Flash-fire', 'Drought']
+  },
+  {
+    name: 'Psyduck',
+    height: 0.8,
+    type: 'Water',
+    eggGroup: ['Water', 'Field'],
+    ability: ['Damp', 'Cloud-nine', 'Swift-swim']
+  },
+  {
+    name: 'Ekans',
+    height: 2,
+    type: 'Poison',
+    eggGroup: ['Dragon', 'Field'],
+    ability: ['Intimidate', 'Shed-skin', 'Unnerve']
+  },
+  {
+    name: 'Pikachu',
+    height: 0.4,
+    type: 'Electric',
+    eggGroup: ['Field', 'Fairy'],
+    ability: ['Static', 'Lightningrod']
+  }
+];
 
-/* Build-in-functions:
--alert()
--document.write() 
--console.log() */
-
-let favoriteFood = 'Sushi';
-document.write(favoriteFood);
