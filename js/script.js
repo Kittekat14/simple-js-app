56ef047913cc5e2e33ff3541982deea91059c250
Katrin Hofstetter <katrin_hofstetter@gmx.de>
2021-07-24 18:04:08 +0200

    'item' as parameter and argument for the showModal function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
-/***Declaring new let pokemonRepository (= IIFE) with functions 'add' and 'getAll' inside to call them later over 'return path' with actual arguments***/
-/**Only executing code if it's the right data type and the expected keys inside the added objects**/
-
 let pokemonRepository = (function() {
-	let modalContainer = document.querySelector('#modal-container');
+	let modalContainer = document.querySelector('#exampleModal');
 	let pokemonList = [];
 	let apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
 
@@ -13,7 +10,7 @@ let pokemonRepository = (function() {
 			Object.keys(pokemon).includes('detailsUrl')) {
 			pokemonList.push(pokemon)
 		} else {
-			console.log('This is no pokemon object. ');
+			console.log('This is no pokemon object.');
 		}
 	}
 
@@ -32,22 +29,17 @@ let pokemonRepository = (function() {
 	}
 
 	function addListItem(pokemon) {
-		let unorderedList = document.querySelector('.list-group');
+		let pokemonList = document.querySelector('.list-group');
 		let listItem = document.createElement('li');
+		listItem.setAttribute('role', 'presentation');
+		//listItem.classList.add('list-group-item');
 		let button = document.createElement('button');
-		button.innerText = pokemon.name;
-		button.classList.add('pokemon-button');
-		button.classList.add('btn');
-		button.classList.add('btn-lg');
-		button.classList.add('text-center');
-		button.classList.add('btn-success');
-		button.classList.add('btn-block');
+		button.classList.add('pokemon-button', 'btn', 'btn-lg', 'text-center', 'btn-block');
 		button.setAttribute('data-toggle', 'modal');
 		button.setAttribute('data-target', '#exampleModal');
-		listItem.setAttribute('role', 'presentation');
-		listItem.classList.add('list-group-item', 'list-group-item-action');
+		button.innerText = pokemon.name;
 		listItem.appendChild(button);
-		unorderedList.appendChild(listItem);
+		pokemonList.appendChild(listItem);
 		button.addEventListener('click', function(event) {
 			showDetails(pokemon); //inside the code-block must be the calling of actually-doing-something-function
 		});
@@ -97,34 +89,29 @@ let pokemonRepository = (function() {
 
 	function showDetails(item) {
 		pokemonRepository.loadDetails(item).then(function() {
-			showModal('Name: ' + item.name.toUpperCase(), 'Height: ' + item.height, 'Types: ' + item.types, 'Abilities: ' + item.abilities, item.imageUrl);
+			showModal(item);
 		});
 	}
 
 
 	function showModal(item) {
-		let modalContainer = document.querySelector('#modal-container');
-		modalContainer.innerHTML = '';
-		let modal = document.querySelector('#exampleModal');
-		modal.classList.add('modal');
-
 		let modalTitle = $('.modal-title');
 		let modalBody = $('.modal-body');
-		let modalHeader = $('.modal-header');
 		modalTitle.empty();
 		modalBody.empty();
 		// Add the new modal content:
-		// creating element for name on modal content
+		// creating name element for modal title
 		let nameElement = $('<h1>' + item.name + '</h1>');
+		//creating image elements for modal body
 		let imageElementFront = $('<img class="modal-img" style="width:50%">');
 		imageElementFront.attr('src', item.imageUrlFront);
 		let imageElementBack = $('<img class="modal-img" style="width:50%">');
 		imageElementBack.attr('src', item.imageUrlBack);
-		// creating element for height, types and abilities:
+		// creating elements for height, types and abilities:
 		let heightElement = $('<p>' + "height : " + item.height + '</p>');
 		let typesElement = $('<p>' + "types : " + item.types + '</p>');
 		let abilitiesElement = $('<p>' + "abilities : " + item.abilities + '</p>');
-
+		
 		modalTitle.append(nameElement);
 		modalBody.append(imageElementFront);
 		modalBody.append(imageElementBack);
@@ -132,47 +119,8 @@ let pokemonRepository = (function() {
 		modalBody.append(typesElement);
 		modalBody.append(abilitiesElement);
 
-
-//existing content of the model:
-let closeButtonElement = document.querySelector('.close');
-closeButtonElement.classList.add('modal-close');
-closeButtonElement.innerText = 'Close';
-closeButtonElement.addEventListener('click', hideModal);
-
-
-modalTitle.append(nameElement);
-modalBody.append(imageElementFront);
-modalBody.append(imageElementBack);
-modalBody.append(heightElement);
-modalBody.append(typesElement);
-modalBody.append(abilitiesElement);
-
-modalContainer.appendChild(modal);
-modalContainer.classList.add('is-visible');
-}
-
-		//Hide Modal function
-		function hideModal() {
-			let modalContainer = document.querySelector('#modal-container');
-			modalContainer.classList.remove('is-visible');
-		}
-
-		//Hiding Modal with ESC key:
-		window.addEventListener('keydown', (e) => {
-			let modalContainer = document.querySelector('#modal-container');
-			if (e.key === 'Escape' &&
-				modalContainer.classList.contains('is-visible')) {
-				hideModal();
-			}
-		});
-		//Hiding Modal with clicking outside the Modal:
-		modalContainer.addEventListener('click', (event) => {
-			let target = event.target;
-			if (target === modalContainer) {
-				hideModal();
-			}
-		});
-
+	}
+		
 		return {
 			add: add,
 			getAll: getAll,
